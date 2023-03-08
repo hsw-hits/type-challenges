38aa4ac9385208364e5f12c490d18c5c24c678c5
Seungwoo Hong <qksn1541@hits.ai>
2023-03-08 18:08:26 +0900

    solve: 4번 문제 해결

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,1 +1,14 @@
-type MyPick<T, K> = any
+type MyPick<T, K extends keyof T> = { [name in K]: T[name] }
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+type TodoPreview = MyPick<Todo, 'title' | 'completed'>
+
+const todo: TodoPreview = {
+  title: 'Clean room',
+  completed: false,
+}
