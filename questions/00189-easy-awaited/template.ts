f61b514956924d2cd8eb904560d1c0f76322878f
Seungwoo Hong <qksn1541@hits.ai>
2023-03-09 12:26:45 +0900

    solve: 189번 해결

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,11 @@
-type MyAwaited<T> = any
+// 이거 쉬움 아닌듯
+
+type MyAwaited<T extends PromiseLike<any>> = T extends PromiseLike<infer U>
+  ? U extends PromiseLike<unknown>
+    ? MyAwaited<U> : U
+  : void
+
+type ExampleType = Promise<string>
+
+type Result = MyAwaited<ExampleType> // string
+
