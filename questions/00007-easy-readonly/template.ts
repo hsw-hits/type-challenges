0dde753c1ffaba04ac4b5728751d8ac7353b6688
Seungwoo Hong <qksn1541@hits.ai>
2023-03-09 11:54:48 +0900

    solve: 7번 문제 해결

--- questions/00007-easy-readonly/template.ts
+++ questions/00007-easy-readonly/template.ts
@@ -1,1 +1,13 @@
-type MyReadonly<T> = any
+type MyReadonly<T> = { readonly [name in keyof T]: T[name] }
+interface Todo {
+  title: string
+  description: string
+}
+
+const todo: MyReadonly<Todo> = {
+  title: 'Hey',
+  description: 'foobar',
+}
+
+todo.title = 'Hello' // Error: cannot reassign a readonly property
+todo.description = 'barFoo' // Error: cannot reassign a readonly property
